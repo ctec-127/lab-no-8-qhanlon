dedef756d57ba49830397f4c4f832d4840c4f8d1
Quinn W. Hanlon <Q.HANLON@labs.clark.edu>
2020-02-26 11:46:45 -0800

    stupidly condensed

---
+++ inc/everything.inc.php
@@ -0,0 +1,8 @@
+<?php 
+require "inc/head.inc.php";
+require "inc/functions.inc.php"; 
+require "inc/anything-you-want.inc.php";
+require "inc/navigation.inc.php";
+require "inc/content.inc.php";
+require "inc/footer.inc.php";
+?>
