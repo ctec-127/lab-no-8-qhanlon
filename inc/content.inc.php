b88ce61d261b41a8f2b390fdce2ea5cc79475a9c
qhanlon <quinn.hanlon@gmail.com>
2020-02-27 23:13:24 -0800

    polished up?

--- inc/content.inc.php
+++ inc/content.inc.php
@@ -8,25 +8,17 @@
     <div id="wrapper">
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="row">
-
-                <div class="section col-12">
-
-                    <div class="col-6">
-                        <label for="temp">Temperature</label>
-                        <input type="text" value="<?php if (isset($_POST['originaltemp'])) echo $_POST['originaltemp'];?>" name="originaltemp" size="7" maxlength="7" id="temp" class="form-control">
-                    </div>
-                    <div class="col-6">
-                        <select name="originalunit" class="form-control selectpicker">
-                            <option value="--Select--" <?php if($originalUnit == "--Select--") echo ' selected="selected"';?>>--Select--</option>
-                            <option value="celsius" <?php if($originalUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
-                            <option value="farenheit" <?php if($originalUnit == "farenheit") echo ' selected="selected"';?>>Farenheit</option>
-                            <option value="kelvin" <?php if($originalUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
-                        </select>
-                    </div>
+                <div class="col-sm-12 col-xs-12 col-md-6">
+                    <label for="temp">Temperature</label>
+                    <input type="text" value="<?php if (isset($_POST['originaltemp'])) echo $_POST['originaltemp'];?>" name="originaltemp" size="7" maxlength="7" id="temp" class="form-control">
+                    <select name="originalunit" class="form-control selectpicker">
+                        <option value="--Select--" <?php if($originalUnit == "--Select--") echo ' selected="selected"';?>>--Select--</option>
+                        <option value="celsius" <?php if($originalUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
+                        <option value="farenheit" <?php if($originalUnit == "farenheit") echo ' selected="selected"';?>>Farenheit</option>
+                        <option value="kelvin" <?php if($originalUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
+                    </select>
                 </div>
-            </div>
-            <div class="row">
-                <div class="section col-6">
+                <div class="section col-xs-12 col-sm-12 col-md-6">
                     <label for="convertedtemp">Converted Temperature</label>
                     <input type="text" value="<?php if (isset($_POST['originaltemp'])) echo round($convertedTemp, 1);?>" name="convertedtemp" size="7" maxlength="7" id="convertedtemp" class="form-control">
 
@@ -36,9 +28,33 @@
                         <option value="farenheit" <?php if($conversionUnit == "farenheit") echo ' selected="selected"';?>>Farenheit</option>
                         <option value="kelvin" <?php if($conversionUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
                     </select>
+                </div> 
+            </div>
+            <br>
+            <input type="submit" value="Convert" class="btn btn-secondary" />
+            <br><br>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <h2>Formulas</h2>
+                    <hr>
+                    <p>
+                    <strong>Celsius (C) -> Fahrenheit: </strong> (C&#730; × 9/5) + 32 <br>
+                    <strong>Celsius (C) -> Kelvin: </strong> C&#730; + 273.15 <br>
+                    <strong>Fahrenheit (F) -> Kelvin: </strong> (F&#730; − 32) × 5/9 + 273.15  <br>
+                    <strong>Fahrenheit (F) -> Celsius: </strong> (F&#730; − 32) × 5/9 <br>
+                    <strong>Kelvin (K) -> Celsius:</strong> K&#730; − 273.15  <br>
+                    <strong>Kelvin (K) -> Fahrenheit:</strong> (K&#730; − 273.15) × 9/5 + 32 
+                    </p>
                 </div>
-                <input type="submit" value="Convert" class="btn btn-secondary" />
-
+                <div class="col-xs-12 col-sm-12 col-md-6">
+                    <h2>Brief Information</h2>
+                    <hr>
+                    <p>
+                        <strong>Celsius</strong> - The standard measurement of temperature used around the world. <br>
+                        <strong>Fahrenheit</strong> - Fewer than a dozen countries in the world still use Fahrenheit because they like alcohol. <br>
+                        <strong>Kelvin</strong> - The measurement used in science because water is a thing.
+                    </p>
+                </div>
             </div>
         </form>
     </div><!-- end wrapper div-->
--- inc/navigation.inc.php
+++ inc/navigation.inc.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5">
   <a class="navbar-brand" href="#">Lab No. 8</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -6,23 +6,13 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="https://smile.amazon.com/">Buy a Thermometer<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="https://www.audubon.org/news/when-bird-birb-extremely-important-guide">Birbs</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="https://www.youtube.com/watch?v=bYyTRqVH_l8">Metal</a>
       </li>
     </ul>
   </div>
